d8ba35d0b478386589c007cfbc638f782c69cb9c
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-11 19:32:23 +0900

    インスタンス作成

--- jp/hashy/finance_app/Main.java
+++ jp/hashy/finance_app/Main.java
@@ -2,6 +2,8 @@ package jp.hashy.finance_app;
 
 class Main {
   public static void main(String[] args) {
-    
+    Finance finance1 = new Finance(510, 40);
+    Finance finance2 = new Finance(540, 140);
+
   }
 }
