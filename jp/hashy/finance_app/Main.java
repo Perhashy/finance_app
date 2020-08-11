2c9d4507c87173b8c53f79be64a89d96b9ed8423
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-11 19:45:16 +0900

    納入数を標準入力

--- jp/hashy/finance_app/Main.java
+++ jp/hashy/finance_app/Main.java
@@ -1,9 +1,18 @@
 package jp.hashy.finance_app;
 
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     Finance finance1 = new Finance(510, 40);
     Finance finance2 = new Finance(540, 140);
+
+
+    System.out.print("会費納入口数を入力してください：");
+    int Count1 = sc.nextInt();
+    System.out.print("追加納入口数を入力してください：");
+    int Count2 = sc.nextInt();
 
   }
 }
