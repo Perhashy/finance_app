7838238ecaaae9ab936c5dfffc16be0658dfc3fa
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-11 19:21:10 +0900

    フィールドとコンストラクタ

--- jp/hashy/finance_app/Finance.java
+++ jp/hashy/finance_app/Finance.java
@@ -1,5 +1,11 @@
 package jp.hashy.finance_app;
 
 class  Finance {
-  
+  private int price;
+  private int campa;
+
+  Finance(int price, int campa) {
+    this.price = price;
+    this.campa = campa;
+  }
 }
